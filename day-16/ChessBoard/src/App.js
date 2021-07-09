dfe9f722e6f78ec880a2aa88d62d59a7f1f40e32
Raman Kumar <rmn5124@gmail.com>
2021-07-09 08:05:33 -0700

    Add files via upload

---
+++ day-16/ChessBoard/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Chess from "./components/Chess";
+function App() {
+  return (
+    <div className="App">
+      <Chess />
+    </div>
+  );
+}
+
+export default App;
---
+++ day-16/ChessBoard/src/components/Chess.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Chess.css";
+const Chess = () => {
+  //   let items = [
+  //     [1, 2, 3, 4, 5, 6, 7, 8],
+  //     [1, 2, 3, 4, 5, 6, 7, 8],
+  //     [1, 2, 3, 4, 5, 6, 7, 8],
+  //     [1, 2, 3, 4, 5, 6, 7, 8],
+  //     [1, 2, 3, 4, 5, 6, 7, 8],
+  //     [1, 2, 3, 4, 5, 6, 7, 8],
+  //     [1, 2, 3, 4, 5, 6, 7, 8],
+  //     [1, 2, 3, 4, 5, 6, 7, 8],
+  //   ];
+  const ver = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  const hor = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  function chess(props) {
+    let board = [];
+    for (let i = 0; i < ver.length - 1; i++) {
+      for (let j = 0; i < hor.length; j++) {
+        const number = j + i + 2;
+        if (number % 2 === 0) {
+          board.push(<div className="tile black"></div>);
+        }
+      }
+    }
+    return <div id="main">{board}</div>;
+  }
+};
+export default Chess;
