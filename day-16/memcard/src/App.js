cbc5c7d95f9cac58abb7ba7db9f0fd36ac4b5804
Raman Kumar <rmn5124@gmail.com>
2021-07-09 08:03:00 -0700

    Add files via upload

---
+++ day-16/memcard/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="card">
+      <img
+        src="https://media.tenor.com/images/a28b86f48a45c8c9abb35f2b1b320323/tenor.gif"
+        alt="logo"
+      />
+      <h1>Meme Card</h1>
+    </div>
+  );
+}
+
+export default App;
